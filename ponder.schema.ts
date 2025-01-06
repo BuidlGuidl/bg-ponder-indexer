de706e96eb2d5f89a5c3be14f501595a4f69df9e
Shiv Bhonde <shivbhonde04@gmail.com>
2025-01-06 09:20:17 +0530

    create index on chohortAddress

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,18 +1,30 @@
-import { onchainTable } from "ponder";
+import { onchainTable, index } from "ponder";
 
-export const cohortBuilder = onchainTable("cohort_builder", (t) => ({
-  id: t.text().primaryKey(),
-  amount: t.real().notNull(),
-  cohortContractAddress: t.hex().notNull(),
-  timestamp: t.bigint().notNull(),
-  ens: t.text(),
-}));
+export const cohortBuilder = onchainTable(
+  "cohort_builder",
+  (t) => ({
+    id: t.text().primaryKey(),
+    amount: t.real().notNull(),
+    cohortContractAddress: t.hex().notNull(),
+    timestamp: t.bigint().notNull(),
+    ens: t.text(),
+  }),
+  (table) => ({
+    cohortAddressIdx: index().on(table.cohortContractAddress),
+  }),
+);
 
-export const cohortWithdrawal = onchainTable("cohort_withdrawal", (t) => ({
-  id: t.text().primaryKey(),
-  builder: t.hex().notNull(),
-  amount: t.real().notNull(),
-  cohortContractAddress: t.hex().notNull(),
-  reason: t.text().notNull(),
-  timestamp: t.bigint().notNull(),
-}));
+export const cohortWithdrawal = onchainTable(
+  "cohort_withdrawal",
+  (t) => ({
+    id: t.text().primaryKey(),
+    builder: t.hex().notNull(),
+    amount: t.real().notNull(),
+    cohortContractAddress: t.hex().notNull(),
+    reason: t.text().notNull(),
+    timestamp: t.bigint().notNull(),
+  }),
+  (table) => ({
+    cohortAddressIdx: index().on(table.cohortContractAddress),
+  }),
+);
